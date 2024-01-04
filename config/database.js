1bcf5a65d40eb10408fff9f5cf36f4b86c7826b3
André Victor <victorandre280@gmail.com>
2024-01-04 20:51:46 -0300

    final

---
+++ config/database.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://127.0.0.1:27017/todolist', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>  console.log('Conectado ao MongoDB'))
+  .catch((err) => console.error(err));
